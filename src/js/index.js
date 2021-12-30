0359ddff53c8ae2238ab7fb9324624bab85f5039
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2021-12-31 01:29:20 +0300

    добавление функции плавного скролла

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 import "../styles/style.css"
-const fixedHeader = require("./functions/scroll")
+const fixedHeader = require("./functions/fixedScroll")
+const smoothScroll = require("./functions/smoothScroll")
 
 $(function () {
   // Фиксированный скролл
   fixedHeader()
+  // Плавный скролл
+  smoothScroll()
 })
