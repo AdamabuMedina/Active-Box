c978f6b6f5079de3f168c0371e006a6b28afd617
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2021-12-31 01:53:27 +0300

    добавление функции burger

--- src/js/functions/smoothScroll.js
+++ src/js/functions/smoothScroll.js
@@ -1,4 +1,4 @@
-const smoothScroll = () => {
+const smoothScroll = (elem) => {
   $("[data-scroll]").on("click", function (event) {
     event.preventDefault()
 
@@ -6,11 +6,14 @@ const smoothScroll = () => {
     const blockID = $this.data("scroll")
     const blockOffset = $(blockID).offset().top
 
+
     $("#nav a").removeClass("active")
     $this.addClass("active")
 
+    elem.removeClass("show")
+
     $("html, body").animate({
-      scrollTop: blockOffset - 58
+      scrollTop: blockOffset - 55
     }, 600)
 
   })
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,15 @@
 import "../styles/style.css"
 const fixedHeader = require("./functions/fixedScroll")
 const smoothScroll = require("./functions/smoothScroll")
+const burgerMenu = require("./functions/burgerMenu")
+
+const nav = $("#nav")
 
 $(function () {
   // Фиксированный скролл
   fixedHeader()
   // Плавный скролл
-  smoothScroll()
+  smoothScroll(nav)
+  // Бургер меню
+  burgerMenu(nav)
 })
