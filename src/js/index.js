87482a4236c486715b71fab18bd881ab33f13934
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2021-12-31 21:14:50 +0300

    добавление функции slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 import "../styles/style.css"
+
 const fixedHeader = require("./functions/fixedScroll")
 const smoothScroll = require("./functions/smoothScroll")
 const burgerMenu = require("./functions/burgerMenu")
+const slider = require("./functions/slider")
 
 const nav = $("#nav")
+const sliderId = $("#reviews__slider")
 
 $(function () {
   // Фиксированный скролл
@@ -12,4 +15,6 @@ $(function () {
   smoothScroll(nav)
   // Бургер меню
   burgerMenu(nav)
+  // Slider
+  slider(sliderId)
 })
